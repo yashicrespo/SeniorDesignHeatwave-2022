ec455ff99561e1595860124e6986db5bb6c527da
yashicrespo <82669659+yashicrespo@users.noreply.github.com>
2022-05-06 23:31:29 -0500

    Add files via upload

---
+++ Mobile App Files/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "heatmap3b44fda5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
